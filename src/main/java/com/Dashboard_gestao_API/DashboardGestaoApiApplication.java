5fa90e1f6ff7ebf20db4b57e14fc8ab80d7b8c4f
Maria Silvano <mariagabrielasilvano@gmail.com>
2024-11-23 13:01:07 -0300

    Inicializando repositório

---
+++ src/main/java/com/Dashboard_gestao_API/DashboardGestaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.Dashboard_gestao_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardGestaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardGestaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dashboard_gestao_API/DashboardGestaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dashboard_gestao_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardGestaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
